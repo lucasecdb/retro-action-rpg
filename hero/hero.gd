d6defde1738c62d1def44578785c36bcac7946b0
Lucas Cordeiro <ecdb.lucas@gmail.com>
2024-10-06 17:47:20 +0200

    Add facing direction value

--- hero/hero.gd
+++ hero/hero.gd
@@ -1,10 +1,26 @@
 extends CharacterBody2D
 
+const SIDE_BIAS := 0.1
+
+var facing_direction: Vector2 = Vector2.DOWN :
+	set(value):
+		if value == Vector2.ZERO:
+			return
+
+		value = value.normalized()
+
+		if abs(value.x) >= abs(value.y) - SIDE_BIAS:
+			facing_direction = Vector2(sign(value.x), 0)
+		else:
+			facing_direction = Vector2(0, sign(value.y))
+
 func _ready() -> void:
 	motion_mode = CharacterBody2D.MOTION_MODE_FLOATING
 
 func _physics_process(delta: float) -> void:
 	var input_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+
+	facing_direction = input_vector
 
 	velocity = input_vector * 100
 
