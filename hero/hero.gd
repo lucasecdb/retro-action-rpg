c522daeee90c03cf357913e376847494834840d8
Lucas Cordeiro <ecdb.lucas@gmail.com>
2024-10-06 23:56:50 +0200

    extract logic into hero move state

--- hero/hero.gd
+++ hero/hero.gd
@@ -1,3 +1,4 @@
+class_name Hero
 extends CharacterBody2D
 
 const SIDE_BIAS := 0.1
@@ -7,7 +8,10 @@ const SIDE_BIAS := 0.1
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var flip_anchor: Node2D = $FlipAnchor
 
-var facing_direction: Vector2 = Vector2.DOWN :
+@onready var move_state := HeroMoveState.new().set_actor(self)
+@onready var fsm := FSM.new().set_state(move_state)
+
+var facing_direction: Vector2 = Vector2.DOWN:
 	set(value):
 		if value == Vector2.ZERO:
 			return
@@ -19,29 +23,20 @@ var facing_direction: Vector2 = Vector2.DOWN :
 		else:
 			facing_direction = Vector2(0, sign(value.y))
 
+
 func _ready() -> void:
 	motion_mode = CharacterBody2D.MOTION_MODE_FLOATING
 
+
 func _physics_process(delta: float) -> void:
-	var input_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	facing_direction = input_vector
+	fsm.state.physics_process(delta)
 
-	if facing_direction.x != 0.0:
-		flip_anchor.scale.x = sign(facing_direction.x)
-
-	if input_vector != Vector2.ZERO:
-		play_animation("run")
-		CharacterMover.accelerate_in_direction(self, input_vector, movement_stats, delta)
-	else:
-		play_animation("idle")
-		CharacterMover.decelerate(self, movement_stats, delta)
-
-	CharacterMover.move(self)
 
 func play_animation(animation: String) -> void:
 	var animation_name := animation + "_" + get_direction_string()
 
 	animation_player.play(animation_name)
+
 
 func get_direction_string() -> String:
 	var direction := ""
