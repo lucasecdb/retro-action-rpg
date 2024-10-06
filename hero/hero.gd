e02f35c15ff08ad4c732d1200188212ade5af266
Lucas Cordeiro <ecdb.lucas@gmail.com>
2024-10-06 17:54:30 +0200

    Add play animation

--- hero/hero.gd
+++ hero/hero.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 const SIDE_BIAS := 0.1
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
 var facing_direction: Vector2 = Vector2.DOWN :
 	set(value):
 		if value == Vector2.ZERO:
@@ -19,9 +21,26 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	var input_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-
+	if input_vector != Vector2.ZERO:
+		play_animation("run")
+	else:
+		play_animation("idle")
 	facing_direction = input_vector
-
 	velocity = input_vector * 100
-
 	move_and_slide()
+
+func play_animation(animation: String) -> void:
+	var animation_name := animation + "_" + get_direction_string()
+
+	animation_player.play(animation_name)
+
+func get_direction_string() -> String:
+	var direction := ""
+	if facing_direction.x == 0.0:
+		if facing_direction.y < 0.0:
+			direction = "up"
+		else:
+			direction = "down"
+	else:
+		direction = "side"
+	return direction
